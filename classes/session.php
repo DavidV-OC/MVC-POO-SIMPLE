1b4169704476265ca0e6d4a7c0ea73e34b2a1287
david <david.viguier46@gmail.com>
2019-01-07 23:01:22 +0100

    classes session en singleton

--- classes/session.php
+++ classes/session.php
@@ -5,20 +5,30 @@
  * Date: 07/01/2019
  * Time: 21:52
  */
-//TODO: Faire un singleton de session
 namespace app;
 
 class session
 {
+    private static $_instance;
+
     public function __construct()
     {
         session_start();
+    }
+
+    public static function getInstance()
+    {
+        if(!self::$_instance){
+            self::$_instance = new session();
+        }
+        return self::$_instance;
     }
 
     public function set($key, $value)
     {
         return $_SESSION[$key] = $value;
     }
+
     public function get($key)
     {
         return $_SESSION[$key];
